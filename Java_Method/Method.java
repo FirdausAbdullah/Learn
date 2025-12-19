26dd9f6e8720047f08c8610473c077d9a7636e1c
FirdausAbdullah <87492065+FirdausAbdullah@users.noreply.github.com>
2025-12-19 20:12:12 +0800

    Notes on Java Method

---
+++ Java_Method/Method.java
@@ -0,0 +1,18 @@
+public class Method{
+
+
+
+    public static void main(String[] args){
+        System.out.println("Method practice");
+
+        //calling a method
+        myMethod();
+        
+        
+    }
+
+    //Declaring a method (must be inside Class Method(in this case))
+        static void myMethod(){
+            System.out.println("myMethod is called.");
+        }
+}
